38acf4eb4b113f2e688c75ef8fb038ae1b7ca250
YouaJing <laizhangheng@qq.com>
2024-09-26 13:30:13 +0800

    新增/team get ID 返回玩家数据

--- src/main/java/tcc/youajing/teamplugin/TeamCommand.java
+++ src/main/java/tcc/youajing/teamplugin/TeamCommand.java
@@ -52,7 +52,10 @@ public class TeamCommand implements CommandExecutor {
                         sender.sendMessage("你还没有加入组织捏");
                         return true;
                     }
-                    sender.sendMessage(teamManager.getTeamByOfflinePlayer(targetPlayer).getName());
+                    String teamName2 = teamManager.getTeamByOfflinePlayer(targetPlayer).getName();
+                    String teamColor = teamManager.getTeamByOfflinePlayer(targetPlayer).getColor().replaceAll("[<>]", "");
+
+                    sender.sendMessage(String.format("{\"team_color\":\"%s\",\"team_name\":\"%s\"}", teamColor, teamName2));
                     return true;
 
 
